e177f166d0444ccfd1e77478521088b6281a4f56
Bartłomiej Sobieszek <contact@protean.pl>
2022-06-03 22:45:54 +0200

    filterwith.ts

--- filterwith.ts
+++ filterwith.ts
@@ -1,5 +1,4 @@
-import TypesUtils from "./TypesUtils";
-const result = [
+const dataset: {}[] = [
   {
     _id: "5e985a07feddae7617ac44f6",
     age: 24,
@@ -17,26 +16,82 @@ const result = [
       },
     ],
   },
+  {
+    _id: "efnb81hf139fh0193",
+    age: 28,
+    eyeColor: "blue",
+    name: "John DOe",
+    gender: "male",
+    company: "Microsoft",
+    email: "mcs@microsoft.com",
+    phone: "+1 (907) 111-2422251",
+    tags: ["nisi", "japaneeese", "mollit", "anim", "aliquip"],
+    friends: [
+      {
+        id: 2,
+        name: "Bruce Jensen",
+      },
+      {
+        id: 3,
+        name: "Shaekspear Bro",
+      },
+    ],
+  },
+  {
+    _id: "qeoifnqoiejfwqe",
+    age: 31,
+    eyeColor: "gray",
+    name: "Joaana Doe",
+    gender: "male",
+    company: "Microsoft",
+    email: "mcs@microsoft.com",
+    phone: "+1 (907) 111-2422251",
+    x: {
+      y: {
+        z: {
+          dude: "Sheppard"
+        }
+      }
+    },
+    tags: ["nisi", "japaneeese", "mollit", "anim", "aliquip"],
+    friends: [
+      {
+        id: 2,
+        name: "Bruce Jensen",
+      },
+      {
+        id: 3,
+        name: "Shaekspear Bro",
+      },
+    ],
+  },
 ];
 
-interface FilterObject {
-  [x: string]: string | number | FilterObject[] | any[];
+const regExpToFindPhrase = (phrase: string): RegExp => new RegExp(`${phrase}`, "gi");
+
+const filterObjectCallback = (value: any, phrase: string): boolean => {
+  let isArray = Array.isArray(value);
+  let isObject = typeof value === "object" && !isArray;
+
+  let isNonPrimitive: boolean = (isArray || isObject);
+
+  if(isNonPrimitive) {
+    if(isObject) {
+      return filterObjectCallback(Object.values(value), phrase);
+    }
+
+    if(isArray) {
+      return value.filter(arrayItemValue => {
+        return filterObjectCallback(arrayItemValue, phrase);
+      }).length > 0;
+    }
+  }
+
+  return regExpToFindPhrase(phrase).test(value);
 }
 
-const filterObject = (
-    object: FilterObject[] | FilterObject,
-    phrase: string
-): {}[] => {
-  const regExpToFindPhrase = new RegExp(`${phrase}`, "gi");
-  const objectToFilter = Object.values(object);
+const result = Object.values(dataset).filter(item => {
+  return filterObjectCallback(item, "Sheppard");
+});
 
-  return objectToFilter.filter((element) => {
-
-        if (TypesUtils.isArray(element) || TypesUtils.isObjectLiteral(element)) {
-          element = filterObject(element, phrase);
-        }
-
-        return regExpToFindPhrase.test(element)
-      }
-  )
-}
+console.log(result);
